6357e26dfeef7b06bf45cfc0314dcf34439f74ba
Dankniel <dankniel@gmail.com>
2020-10-06 14:06:59 -0300

    Version 1.0 - Reto #1 listo

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "\n***************************************************\n";
+echo "*             Retos de programación - PHP         *\n";
+echo "*         Curso de fundamentos de PHP de Platzi   *\n";
+echo "***************************************************\n";
+echo "\nEstudiante: Daniel Contreras\n\n\n";
+
+#Reto 1
+echo"*******************\n";
+echo"*      Reto #1    *\n";
+echo"*******************\n\n";
+
+$nombre = readline('Ingresar nombre: >');
+echo "\nHola, " . $nombre . "\n";
